29c21c4769ba45d524f3b6920771b74f7a223861
Will Rigby-Hall <williamgrh@gmail.com>
2023-02-28 11:50:31 -0500

    fix kill port

--- cypress-clean.sh
+++ cypress-clean.sh
@@ -2,20 +2,16 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Kill Process on Port
+# @raycast.title Cypress Clean
 # @raycast.mode silent
 
 # Optional parameters:
 # @raycast.icon 🤖
-# @raycast.argument1 { "type": "text", "placeholder": "Port number" }
 
 # Documentation:
-# @raycast.description Kill processes running on a given port
+# @raycast.description Cleanup test artifacts
 # @raycast.author Will Rigby-Hall
 # @raycast.authorURL https://github.com/williamgrh
 
-pid=$(lsof -t -i:"$1")
-if [ $pid ]
-then
-  kill -9 $pid
-fi
+rm -rf ~/git/solo/gloo-mesh-enterprise/gloo-mesh-ui/ui/test_results
+rm -rf ~/git/solo/gloo-mesh-enterprise/gloo-mesh-ui/ui/cypress/test_results
